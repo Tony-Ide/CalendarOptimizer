c35912a199380549f23363b511e320c8ef0675ec
JoshuaJJeong <josh15685@yahoo.com>
2023-05-25 23:19:39 -0700

    Sprint 2: Add todo class

--- header/todo.hpp
+++ header/todo.hpp
@@ -1,13 +1,23 @@
 #ifndef TODO_HPP
 #define TODO_HPP
+#include "date.hpp"
+#include <string.h>
+
 
 class {
     private:
-
-
+        Date date;
+        string name;
+        int priority;
+        int time;
+        int duration;
     public:
-
-
-}
+        Todo(Date d, string s, int p, int t, int dur);
+        Date getDate();
+        string getName();
+        int getPriority();
+        int getTime();
+        int getDuration();
+};
 
 #endif // TODO_HPP
