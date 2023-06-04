b2e936e6808f700677e16ec6eec70e764c5cb686
github-username <lspyo11@gmail.com>
2023-06-03 22:18:56 -0700

    made week print

--- src/main_menu.cpp
+++ src/main_menu.cpp
@@ -27,14 +27,94 @@ using namespace std;
     }
 
 
-    void MainMenu::printWeek(string date){
-
-    }
-    void MainMenu::printDay(std::string date){
-
-        vector<string> dateSplit = split(date,'/');
+    void MainMenu::printWeek(Date* targetDate){
+        /*vector<string> dateSplit = split(date,'/');
         int month = stoi(dateSplit[0]);
-        int day = stoi(dateSplit[1]);
+        int day = stoi(dateSplit[1]);*/
+        int month = targetDate->getMonth();
+        int day = targetDate->getMonth();
+        int m[]= { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        vector<int> printdates[7];
+        
+        for(int i = -3; i<4;i++){
+            if(day + i <= 0){
+                if(month==1) printdates[i+3].push_back(12);
+                else printdates[i+3].push_back(month-1);
+
+                printdates[i+3].push_back(day+i+m[month-1]);
+            }
+            else if(day+i>m[month]){
+                if(month == 12) printdates[i+3].push_back(1);
+                else printdates[i+3].push_back(month+1);
+                printdates[i+3].push_back(day+i-m[month]);
+            }
+            else{
+                printdates[i+3].push_back(month);
+                printdates[i+3].push_back(day+i);
+            }
+        }
+        
+        
+        list<Todo*> newTodoList = scheduler->getTaskList();
+        list<Todo*>::iterator todoIterater;
+        vector<string> linePrint[7][51];//<printing Line>
+
+        printf("|       |");
+        for(int days=0;days<7;days++){
+            printf("    %2i/%2i    |",printdates[days][0],printdates[days][1]);
+        }
+        printf("\n");
+        for(int hour=6*2;hour<25*2;hour++){
+            for(todoIterater = newTodoList.begin(); todoIterater!= newTodoList.end(); todoIterater++){
+                for(int days=0;days<7;days++){
+                    if((*todoIterater) ->getDate()->getMonth() == printdates[days][0] && (*todoIterater)->getDate()->getDay() == printdates[days][1]
+                        && (*todoIterater)->getTime() >= hour * 50 && (*todoIterater)->getTime() < hour*50 + 30){
+                            
+                        for(int i=0;i<(*todoIterater)->getDuration()*2+1;i++){
+                            if(hour+i > 50){
+                                break;
+                            }
+                            if(i == 0|| i == (*todoIterater)->getDuration()*2){
+                                if(linePrint[days][hour+i].size() == 0){
+                                    linePrint[days][hour+i].push_back("-------------");
+                                }
+                            }
+                            else if (i == ((*todoIterater)->getDuration()*2+1)/2){
+                                linePrint[days][hour+i].push_back((*todoIterater)->getName());
+                            }
+                        }
+                        
+                    }
+                }
+            }
+        }
+        
+        for(int hour=6*2;hour<25*2;hour++){
+            if(hour%2 ==0){printf("| %2i:00 |",hour/2);}
+            else {printf("|       |");}
+            for(int days=0;days<7;days++){
+                if(linePrint[days][hour].size() != 0){
+                    for(int i=0;i<linePrint[days][hour].size();i++){
+                        printf("%13s",linePrint[days][hour][i].c_str());
+                    }
+                    printf("|");
+                }
+                else{
+                    printf("             |");
+                }
+            }
+            printf("\n");
+        }
+        
+    }
+
+    void MainMenu::printDay(Date* targetDate){
+
+        /*vector<string> dateSplit = split(date,'/');
+        int month = stoi(dateSplit[0]);
+        int day = stoi(dateSplit[1]);*/
+        int month = targetDate->getMonth();
+        int day = targetDate->getMonth();
         list<Todo*> newTodoList = scheduler->getTaskList();
         list<Todo*>::iterator todoIterater;
 
@@ -42,8 +122,7 @@ using namespace std;
         int lastLineNum=0;
         int printline=0;
         int todoExists=false;
-        printf("%13s\n",date.c_str());
-        printf("|---------------------|\n");
+        printf("          %2i/%2i\n",month,day);
         for(int hour=6*2;hour<25*2;hour++){
             for(todoIterater = newTodoList.begin(); todoIterater!= newTodoList.end(); todoIterater++){
                 if((*todoIterater)->getDate()->getMonth() == month && (*todoIterater)->getDate()->getDay() == day
